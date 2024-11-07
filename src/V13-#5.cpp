04d34948e028876159d50644c90e34df068ce1b7
IghtDoe <1almightynail@gmail.com>
2024-11-08 00:31:31 +0300

    V13-#5.cpp

--- src/V13-#5.cpp
+++ src/V13-#5.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
     setlocale(LC_ALL,"RU");
 
-    int x, y, n;
-    cout << "Введите координаты x, y:"; cin >> x >> y;
+    int n;
+    cout << "Введите кол-во точек: "; cin >> n;
 
+    int count = 0;
 
-    cout <<"Кол-во точек: " << n;
-
+    for (int i = 0; i < n; ++i) {
+        double x,y;
+        cout << "Введите координаты x, y: " << i + 1 << ":"; cin >> x >> y;
     
+    if(x >= 0 && x <= 4 && y >= 0 && y <= -1/2*x + 4
+    || x <= 0 && x >= -4 && (x*x + y*y) <= 64
+    || x >= 0 && y <= 0 && (x*x + y*y <= 64)) {
+        count++;
+        }
+    }
+
+    cout << "Кол-во точек в заштрихованной области: " << count << endl;
+
     return 0;
 }
